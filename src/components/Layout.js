017872e6b2153a5e98a00c946a5354b1d7a176c5
Jonathan Cutrell <jonathan@clearbit.com>
2019-03-31 19:22:45 -0400

    updating some styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -62,7 +62,7 @@ class Layout extends React.Component {
           style={{
             marginLeft: `auto`,
             marginRight: `auto`,
-            maxWidth: rhythm(26),
+            maxWidth: rhythm(24),
             padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
           }}
         >
